76660c4a7e16e1c85ecf54278b8c290c28837bcc
laxat <fatman7100@gmail.com>
2020-02-25 19:56:15 -0500

    AR-40 This commit adds connectivity to the application

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:arezue/splashScreen.dart';
 import 'package:arezue/introduction.dart';
 import 'package:arezue/provider.dart';
 import 'package:arezue/services/auth.dart';
+import 'package:provider/provider.dart';
 
 bool leading = true;
 
@@ -13,24 +14,16 @@ class Launcher extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-          theme: ThemeData(
-              primaryColor: ArezueColors.outPrimaryColor,
-              accentColor: ArezueColors.outSecondaryColor),
-          debugShowCheckedModeBanner: false,
-          home: Splash(auth: new Auth()),
+      theme: ThemeData(
+          primaryColor: ArezueColors.outPrimaryColor,
+          accentColor: ArezueColors.outSecondaryColor),
+      debugShowCheckedModeBanner: false,
+      home: Splash(auth: new Auth()),
+
     );
 
-//    return Provider(
-//      auth: Auth() ,
-//      child: MaterialApp(
-//          theme: ThemeData(
-//              primaryColor: ArezueColors.outPrimaryColor,
-//              accentColor: ArezueColors.outSecondaryColor),
-//          debugShowCheckedModeBanner: false,
-//          home: Splash(),
-//          routes: routes),
-//    );
-  }
+}
 
 }
--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,10 +1,13 @@
 import 'package:arezue/services/auth.dart';
 import 'package:arezue/introduction.dart';
 import 'package:arezue/jobseeker/HomePage.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:arezue/utils/colors.dart';
 import 'package:arezue/utils/images.dart';
 import 'package:arezue/utils/texts.dart';
+import 'package:provider/provider.dart';
 import 'dart:async';
 
 class Splash extends StatefulWidget {
@@ -20,55 +23,165 @@ enum AuthStatus {
   signedIn,
 }
 
+enum ConnectivityStatus{
+  Wifi,
+  Cell,
+  Offline
+}
+
 class _SplashScreenState extends State<Splash> {
 
   AuthStatus authStatus = AuthStatus.notSignedIn;
+  StreamSubscription<DataConnectionStatus> listener;
   Timer timer;
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
-    widget.auth.currentUser().then((userId) async{
+    widget.auth.currentUser().then((userId) async {
       bool flag = (await widget.auth.checkEmailVerification()) == true;
-      setState((){
-        authStatus = (userId != null) && flag ? AuthStatus.signedIn : AuthStatus.notSignedIn;
+      setState(() {
+        authStatus =
+        (userId != null) && flag ? AuthStatus.signedIn : AuthStatus.notSignedIn;
         print(authStatus);
       });
     });
-    timer = new Timer(Duration(seconds: 5), () => check());
-
+    timer = new Timer(Duration(seconds: 5), () => _check_network());
   }
 
   @override
   void dispose() {
+    listener.cancel();
     timer.cancel();
   }
 
-  Widget check(){
+  Widget check() {
     switch (authStatus) {
-        case AuthStatus.notSignedIn:
-          Navigator.pop(context);
-          Navigator.push(context,
-            MaterialPageRoute(builder: (context) => Intro(
-              auth: widget.auth,
-              onSignIn: () => _updateAuthStatus(AuthStatus.signedIn),
-            ), fullscreenDialog: true),
-          );
-          break;
+      case AuthStatus.notSignedIn:
+        Navigator.pop(context);
+        Navigator.push(context,
+          MaterialPageRoute(builder: (context) =>
+              Intro(
+                auth: widget.auth,
+                onSignIn: () => _updateAuthStatus(AuthStatus.signedIn),
+              ), fullscreenDialog: true),
+        );
+        break;
 
-        case AuthStatus.signedIn:
-          Navigator.pop(context);
-          Navigator.push(context,
-            MaterialPageRoute(builder: (context) => JobseekerHomePage(
-              auth: widget.auth,
-              onSignOut: () => _updateAuthStatus(AuthStatus.notSignedIn),
-            ), fullscreenDialog: true),
-          );
-          break;
-
-      }
+      case AuthStatus.signedIn:
+        Navigator.pop(context);
+        Navigator.push(context,
+          MaterialPageRoute(builder: (context) =>
+              JobseekerHomePage(
+                auth: widget.auth,
+                onSignOut: () => _updateAuthStatus(AuthStatus.notSignedIn),
+              ), fullscreenDialog: true),
+        );
+        break;
+    }
   }
 
+  _check_internet() async {
+
+    print("The statement 'this machine is connected to the Internet' is: ");
+    print(await DataConnectionChecker().hasConnection);
+    // returns a bool
+
+    // We can also get an enum value instead of a bool
+    print("Current status: ${await DataConnectionChecker().connectionStatus}");
+    // prints either DataConnectionStatus.connected
+    // or DataConnectionStatus.disconnected
+
+    // This returns the last results from the last call
+    // to either hasConnection or connectionStatus
+    print("Last results: ${DataConnectionChecker().lastTryResults}");
+
+
+    listener = DataConnectionChecker().onStatusChange.listen((status) {
+      switch (status) {// actively listen for status updates
+      // this will cause DataConnectionChecker to check periodically
+      // with the interval specified in DataConnectionChecker().checkInterval
+      // until listener.cancel() is called
+        case DataConnectionStatus.connected:
+          print('Data connection is available.');
+          break;
+        case DataConnectionStatus.disconnected:
+          print('You are disconnected from the internet.');
+          break;
+      }
+    });
+
+    return await DataConnectionChecker().connectionStatus;
+  }
+
+  _check_network() async {
+    var result = await Connectivity().checkConnectivity();
+    var connectionStatus = _getStatus(result);
+
+    DataConnectionStatus int_status = await _check_internet();
+
+    if (connectionStatus == ConnectivityStatus.Offline) {
+      _showDialog('No Internet',
+                  "No connection to internat established");
+    } else if (connectionStatus == ConnectivityStatus.Wifi) {
+
+      if (int_status == DataConnectionStatus.connected){
+        check();
+      }else {
+        _showDialog("No Internet",
+                    "Cannot reach database");
+      }
+    } else if (connectionStatus == ConnectivityStatus.Cell) {
+      if(int_status == DataConnectionStatus.connected) {
+        check();
+      }
+      else {
+        _showDialog("No Internet",
+            "Cannot reach database");
+      }
+
+    }
+  }
+
+  _showDialog(title, text) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(title),
+            content: Text(text),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Try Again'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _check_network();
+                },
+              )
+            ],
+          );
+        }
+    );
+  }
+
+  ConnectivityStatus _getStatus(ConnectivityResult result) {
+
+    switch (result) {
+      case ConnectivityResult.mobile:
+        return ConnectivityStatus.Cell;
+
+      case ConnectivityResult.wifi:
+        return ConnectivityStatus.Wifi;
+
+      case ConnectivityResult.mobile:
+        return ConnectivityStatus.Offline;
+
+      default:
+        return ConnectivityStatus.Offline;
+    }
+  }
 
   void _updateAuthStatus(AuthStatus status) {
     setState(() {
